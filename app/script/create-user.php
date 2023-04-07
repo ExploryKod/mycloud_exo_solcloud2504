59958f624e94e4c5f1e6048867ea86fbe553f56a
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 15:42:51 +0200

    Update create-user.php

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -17,3 +17,21 @@ shell_exec("./createbdd.sh $username $password");
 echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 
 shell_exec("./restartNginx.sh");
+
+$host = "localhost";
+$user = "root";
+$password = "new_password";
+$dbname = "users";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+$sql = "INSERT INTO users (username, password, ssh) 
+VALUES ($username, $password, $ssh)";
+
+// Execute query
+if (mysqli_query($conn, $sql)) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
