7ae2afc45e5365c84a1971bb8365f2ce597d4aae
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-04-07 15:56:56 +0200

    Update create-user.php

--- app/script/create-user.php
+++ app/script/create-user.php
@@ -2,6 +2,26 @@
 
 $username = $_GET['username'];
 $password = $_GET['password'];
+
+$host = "localhost";
+$user = "root";
+$passwordBdd = "new_password";
+$dbname = "users";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $passwordBdd, $dbname);
+
+$sql = "INSERT INTO users (username, password, ssh) 
+VALUES ($username, $password, $ssh)";
+
+// Execute query
+if (mysqli_query($conn, $sql)) {
+    echo "Table users created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+
 $ssh = $_GET['ssh'];
 
 $file = fopen("authorized_keys", "w");
@@ -17,21 +37,3 @@ shell_exec("./createbdd.sh $username $password");
 echo "<h1 style='color: green;'>Le script pour créer le compte de <strong style='color: black'>$username</strong> a été appelé ! </h1>";
 
 // shell_exec("./restartNginx.sh");
-
-$host = "localhost";
-$user = "root";
-$password = "new_password";
-$dbname = "users";
-
-// Create connection
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-$sql = "INSERT INTO users (username, password, ssh) 
-VALUES ($username, $password, $ssh)";
-
-// Execute query
-if (mysqli_query($conn, $sql)) {
-    echo "Table users created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
